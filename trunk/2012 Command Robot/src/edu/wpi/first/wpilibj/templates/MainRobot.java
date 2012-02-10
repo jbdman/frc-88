d1a7c52fe5bde1e0696e901464b9a8a7b60cfd8f
DWigley92@gmail.com <DWigley92@gmail.com@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2012-02-10 22:11:37 +0000

    TEST to see if commit works

--- trunk/2012 Command Robot/src/edu/wpi/first/wpilibj/templates/MainRobot.java
+++ trunk/2012 Command Robot/src/edu/wpi/first/wpilibj/templates/MainRobot.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.templates.subsystems.Pitcher;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
+ * David Wigley was here.
  */
 public class MainRobot extends IterativeRobot {
 
