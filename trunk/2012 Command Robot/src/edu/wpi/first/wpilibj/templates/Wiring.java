d1afcaee176364533be09e08db6e0cb271fcb418
christopher.edgington <christopher.edgington@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2012-02-11 00:51:19 +0000

    Added more subsystems, and edited other classes as were necessary

--- trunk/2012 Command Robot/src/edu/wpi/first/wpilibj/templates/Wiring.java
+++ trunk/2012 Command Robot/src/edu/wpi/first/wpilibj/templates/Wiring.java
@@ -10,21 +10,36 @@ public class Wiring {
     /*
      * ### CAN Motors ###
      */
+    public static final int landingGearCANID     =  5;    // FIX THIS!
 
-    // Drive system
-    public static final int driveFrontLeftCANID  =  9;    // FIX THIS!
-    public static final int driveFrontRightCANID = 10;    // FIX THIS!
-    public static final int driveRearLeftCANID   = 11;    // FIX THIS!
-    public static final int driveRearRightCANID  = 12;    // FIX THIS!
-    // Pitcher
-    public static final int pitcherUpperMotorCANID = 2;
-    public static final int pitcherLowerMotorCANID = 5;
+    //String theory
+    public static final int liftingStringsCANID  =  8;    // FIX THIS!
+    public static final int right_leftStringCanID=  7;    // FIX THIS!
+    public static final int front_backStingCANID =  6;    // FIX THIS!
 
+    // Drive system(motors and encoders
+    public static final int driveFrontLeftCANID   =  9;    // FIX THIS!
+    public static final int driveFrontLeftEncoder =  1;    // FIX THIS!
+    public static final int driveFrontRightCANID  = 10;    // FIX THIS!
+    public static final int driveFrontRightEncoder=  2;    // FIX THIS!
+    public static final int driveRearLeftCANID    = 11;    // FIX THIS!
+    public static final int driveRearLeftEncoder  =  3;    // FIX THIS!
+    public static final int driveRearRightCANID   = 12;    // FIX THIS!
+    public static final int driveRearRightEncoder =  4;    // FIX THIS!
+
+    //Turret
+    public static final int pitcherUpperMotorCANID= 2;    // FIX THIS!
+    public static final int pitcherLowerMotorCANID= 3;    // FIX THIS!
+    public static final int turretSpinnerCANID    = 4;    // FIX THIS!
+    
+    //Analog
+    public static final int turretRotationEncoder  =  5;    // FIX THIS!
+    public static final int robotPositionAcelometer=  9;    // FIX THIS!
+    public static final int robotPositionGyro      = 10;    // FIX THIS!
     /*
      * ### Digital IO ports ###
      */
-    // Pitcher
-    public static final int pitcherUpperSpeedSensor = 1;
-    public static final int pitcherLowerSpeedSensor = 2;
-
+    public static final int pitcherUpperSpeedSensor = 6;    // FIX THIS!
+    public static final int pitcherLowerSpeedSensor = 7;    // FIX THIS!
+    public static final int pneumaticPressureSwitch = 8;    // FIX THIS!
 }
