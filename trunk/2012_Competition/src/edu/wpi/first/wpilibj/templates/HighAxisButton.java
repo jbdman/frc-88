74accebd15148f755444d7273e8e5c07a128630a
mike.edgington <mike.edgington@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2012-03-08 14:13:28 +0000

    Added classes to map a joystick axis to a button Moved string theory to driver trigger buttons

---
+++ trunk/2012_Competition/src/edu/wpi/first/wpilibj/templates/HighAxisButton.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * Logical button based on the value of the joystick axis.
+ *
+ * @author Mike_Edgington
+ */
+public class HighAxisButton extends Button {
+
+    private Joystick joystick;
+    private int axis = 3;
+    private double trigger = 0.5;
+
+    /**
+     * Logical button based on the value of the joystick axis.
+     * Uses defaults of axis 3 (L/R triggers on XBox controller) and 50% for trigger value
+     *
+     * @param stick the joystick to read
+     *
+     */
+    public HighAxisButton(Joystick stick) {
+        joystick = stick;
+    }
+
+    /**
+     * Logical button based on the value of the joystick axis.
+     * Uses default of 50% for trigger value
+     *
+     * @param stick the joystick to read
+     * @param axis the raw axis to read
+     *
+     */
+    public HighAxisButton(Joystick stick, int axis) {
+        joystick = stick;
+        this.axis = axis;
+    }
+
+    /**
+     * Logical button based on the value of the joystick axis
+     *
+     * @param stick the joystick to read
+     * @param axis the raw axis to read
+     * @param trigger the trigger value - true if greater than this value
+     *
+     */
+     public HighAxisButton(Joystick stick, int axis, double trigger) {
+        joystick = stick;
+        this.axis = axis;
+        this.trigger = trigger;
+    }
+
+    /**
+     * Returns logical button value depending on the value of the joystick axis
+     *
+     * @return true if the axis is greater than the trigger value
+     */
+    public boolean get() {
+        return (joystick.getRawAxis(axis) > trigger);
+    }
+}
