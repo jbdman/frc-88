45eae7f359853e5b7fcdd4b17df1e3630c18b10b
DWigley92@gmail.com <DWigley92@gmail.com@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2013-01-16 00:24:42 +0000

    The 2013 robot as of 1/15/13

--- 2013 Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Drive.java
+++ 2013 Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Drive.java
@@ -28,7 +28,6 @@ public class Drive extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public Drive()  {
-        //dont understand these just copy pasted in effort to get rid of errors. I know they're necessary though - David
         
         }
         
@@ -37,43 +36,74 @@ public class Drive extends Subsystem {
         // Two jags commented out because they don't have encoders plugged in, must be uncommented for speed control
             try {
                 frontLeftJag = new CANJaguar(12);
+                // frontLeftJag.setPID(0.005,0.02,0);
             }
             catch (CANTimeoutException ex) {
-                // error
             }
-            // frontLeftJag.setPID(0.005,0.02,0);
             try {
                 frontRightJag = new CANJaguar(5);
             }
             catch  (CANTimeoutException ex) {
-                
             }
             try {
                 frontRightJag.setPID(0.005,0.02,0);
             }
             catch  (CANTimeoutException ex) {
-                
             }
             try {
                 backLeftJag = new CANJaguar(17);
             }
             catch (CANTimeoutException ex) {
-                
             }
-             try {
-                 backLeftJag.setPID(0.005,0.02,0);
-             }
-             catch (CANTimeoutException ex) {
-                 
-             }
+            try {
+                backLeftJag.setPID(0.005,0.02,0);
+            }
+            catch (CANTimeoutException ex) {
+            }
             try {
                 backRightJag = new CANJaguar(16);
             }
             catch  (CANTimeoutException ex)  {
-                
             }
             // backRightJag.setPID(0.005,0.02,0);
+            
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+        public void driveTankOpenLoop(double left, double right) {
+
+        if(frontLeftJag != null) {
+            try {
+                frontLeftJag.setX(-left);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(frontRightJag != null) {
+            try {
+                frontRightJag.setX(right);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(backLeftJag != null) {
+            try {
+                backLeftJag.setX(-left);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(backRightJag != null) {
+            try {
+                backRightJag.setX(right);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
     }
 }
