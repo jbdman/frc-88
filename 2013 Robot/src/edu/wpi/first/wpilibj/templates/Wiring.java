41c66fd1668a0a2c073d1da940487d118768d7d9
DWigley92@gmail.com <DWigley92@gmail.com@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2013-01-16 00:52:19 +0000

    David + Ag work

--- 2013 Robot/src/edu/wpi/first/wpilibj/templates/Wiring.java
+++ 2013 Robot/src/edu/wpi/first/wpilibj/templates/Wiring.java
@@ -11,43 +11,8 @@ public class Wiring {
      * ### CAN Motors ###
      */
     // Drive system (motors and encoders)
-    public static final int driveFrontLeftCANID    = 10;
-    public static final int driveFrontRightCANID   = 11;
-    public static final int driveRearLeftCANID     = 8;
-    public static final int driveRearRightCANID    = 2;
-
-    //String theory
-    public static final int lifterMotorCANID  =  9;
-
-    //RampPusher
-    public static final int rampPushingMotorCANID  = 6;
-
-    //Turret
-    public static final int pitcherUpperMotorCANID = 3;
-    public static final int pitcherLowerMotorCANID = 7;
-    public static final int turretMotorCANID       = 5;
-
-    /*
-     * ### Solenoids ###
-     */
-    public static final int shooterLoadSolenoid    = 4;
-    public static final int shooterAngleSolenoid   = 5;
-    public static final int footSolenoid           = 6;
-
-    // Analog
-    public static final int robotPositionAccelerometer =  1;    // not used yet
-    public static final int robotPositionGyro      = 2;     // not used yet
-
-    /*
-     * ### Digital Relay ports ###
-     */
-    public static final int compressorPowerRelay = 8;
-
-    /*
-     * ### Digital IO ports ###
-     */
-    public static final int compressorPressureSwitch = 3;
-    public static final int turretLimitSwitch = 14;
-    public static final int rampPusherMainLimitSwitch = 1;
-    public static final int rampPusherDownSwitch = 5;
+    public static final int driveFrontLeftCANID    = 12;
+    public static final int driveFrontRightCANID   = 5;
+    public static final int driveBackLeftCANID     = 17;
+    public static final int driveBackRightCANID    = 16;
 }
--- 2013 Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Drive.java
+++ 2013 Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Drive.java
@@ -35,43 +35,42 @@ public class Drive extends Subsystem {
         //David reqs. help
         // Two jags commented out because they don't have encoders plugged in, must be uncommented for speed control
             try {
-                frontLeftJag = new CANJaguar(12);
-                // frontLeftJag.setPID(0.005,0.02,0);
+                frontLeftJag = new CANJaguar(Wiring.driveFrontLeftCANID);
+                //frontLeftJag.setPID(0.005,0.02,0);
             }
             catch (CANTimeoutException ex) {
             }
             try {
-                frontRightJag = new CANJaguar(5);
-            }
-            catch  (CANTimeoutException ex) {
-            }
-            try {
+                frontRightJag = new CANJaguar(Wiring.driveFrontRightCANID);
+                // Need to determine encoder codes per rev
+                frontRightJag.configEncoderCodesPerRev(360);
+                frontRightJag.setPositionReference(CANJaguar.PositionReference.kQuadEncoder);
+                frontRightJag.changeControlMode(CANJaguar.ControlMode.kPosition);
                 frontRightJag.setPID(0.005,0.02,0);
             }
             catch  (CANTimeoutException ex) {
             }
             try {
-                backLeftJag = new CANJaguar(17);
-            }
-            catch (CANTimeoutException ex) {
-            }
-            try {
+                backLeftJag = new CANJaguar(Wiring.driveBackLeftCANID);
+                backLeftJag.configEncoderCodesPerRev(360);
+                backLeftJag.setPositionReference(CANJaguar.PositionReference.kQuadEncoder);
+                backLeftJag.changeControlMode(CANJaguar.ControlMode.kPosition);
                 backLeftJag.setPID(0.005,0.02,0);
             }
             catch (CANTimeoutException ex) {
             }
             try {
-                backRightJag = new CANJaguar(16);
+                backRightJag = new CANJaguar(Wiring.driveBackRightCANID);
+                //backRightJag.setPID(0.005,0.02,0);
             }
             catch  (CANTimeoutException ex)  {
             }
-            // backRightJag.setPID(0.005,0.02,0);
             
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
-        public void driveTankOpenLoop(double left, double right) {
+    public void driveTankOpenLoop(double left, double right) {
 
         if(frontLeftJag != null) {
             try {
@@ -100,6 +99,43 @@ public class Drive extends Subsystem {
         if(backRightJag != null) {
             try {
                 backRightJag.setX(right);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+    }
+    
+    public void driveTankClosedLoop(double left, double right) {
+        // Change to max speed wanted
+        int maxRPM = 100;
+        if(frontLeftJag != null) {
+            try {
+                frontLeftJag.setX(-left * maxRPM);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(frontRightJag != null) {
+            try {
+                frontRightJag.setX(right * maxRPM);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(backLeftJag != null) {
+            try {
+                backLeftJag.setX(-left * maxRPM);
+            } catch(CANTimeoutException ex) {
+                m_fault = true;
+                System.err.println("****************CAN timeout***********");
+            }
+        }
+        if(backRightJag != null) {
+            try {
+                backRightJag.setX(right * maxRPM);
             } catch(CANTimeoutException ex) {
                 m_fault = true;
                 System.err.println("****************CAN timeout***********");
