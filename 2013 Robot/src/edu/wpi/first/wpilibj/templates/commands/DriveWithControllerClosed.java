0a52b3069924be60508c59c1b5f61e5eec0a273f
mike.edgington@gmail.com <mike.edgington@gmail.com@9b2a5f7d-5703-da21-c39b-9aa029d848f4>
2013-03-01 03:34:44 +0000

    Fixed small bug in ramp rate and increased limit (i.e. decreased delta change)

--- 2013 Robot/src/edu/wpi/first/wpilibj/templates/commands/DriveWithControllerClosed.java
+++ 2013 Robot/src/edu/wpi/first/wpilibj/templates/commands/DriveWithControllerClosed.java
@@ -17,7 +17,7 @@ public class DriveWithControllerClosed extends CommandBase {
     
 //    private static final double BRAKE_THRESH = 0.5;
     private static final double MAX_SPEED = 80.0;
-    private static final double RAMP_RATE = 20.0;
+    private static final double RAMP_RATE = 10.0;
 
     private boolean m_brake = false;
     
@@ -65,19 +65,21 @@ public class DriveWithControllerClosed extends CommandBase {
         left = MAX_SPEED * left;
         right = MAX_SPEED * right;
 
+        // limit the change in setpoint between calls
+        // this is not a true ramp rate since we don't correct for the rate of update
+        // it should also be implemented in the subsystem
         if(left - last_left > RAMP_RATE) {
             left = last_left + RAMP_RATE;
         } else if(left - last_left < -RAMP_RATE) {
             left = last_left - RAMP_RATE;
         }
-        
         if(right - last_right > RAMP_RATE) {
             right = last_right + RAMP_RATE;
         } else if(right - last_right < -RAMP_RATE) {
             right = last_right - RAMP_RATE;
         }
 
-        drive.driveTankClosedLoop((left + last_left)/2, (right+last_right)/2);
+        drive.driveTankClosedLoop(left, right);
         last_left = left;
         last_right = right;
         
